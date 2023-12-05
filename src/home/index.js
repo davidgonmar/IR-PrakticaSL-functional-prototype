44f73e02b925cd51532866ee9afc49351ea537d0
Sergiotg03 <sergiotg003@gmail.com>
2023-12-05 04:23:43 +0100

    Provisional home page

--- src/home/index.js
+++ src/home/index.js
@@ -5,8 +5,9 @@ import '../static/css/home/home.css';
 export default function Home() {
   return (
     <div className='home-page-container'>
-      <div className='hero-div'>
-        <h1>Praktica SL</h1>
+      <div style={{ height: '600px', textAlign: 'center' }}>
+        <img style={{ width: '500px' }} src={require("../static/images/praktica.png")} alt='Praktica Logo' className='logo-img' />
+        <p style={{ fontSize: '44px', marginTop: '70px', color: 'white' }}>Welcome!</p>
       </div>
     </div>
   );
