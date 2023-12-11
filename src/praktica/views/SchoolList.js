b8c2e9f594f5a2e923aac18e5538ff8338d2bd5c
David González Martínez <me@davidgonmar.me>
2023-12-11 18:26:44 +0100

    fix: school list

--- src/praktica/views/SchoolList.js
+++ src/praktica/views/SchoolList.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { ReusableList } from '../common/ReusableList';
 
 const schools = ['School1', 'School2', 'School3', 'School4', 'School5'];
-export function SchoolList({ schools }) {
+
+export function SchoolList() {
   const [items, setItems] = React.useState(schools);
 
-  console.log(items);
   return (
     <div className='container'>
-      <ReusableList items={items} />
+      <ReusableList items={schools} />
     </div>
   );
 }
