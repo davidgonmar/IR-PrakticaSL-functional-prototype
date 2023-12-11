c5c9d1c0e7b9ebf1040dd0df8d580ae6cbf2d5ca
David González Martínez <me@davidgonmar.me>
2023-12-11 19:14:57 +0100

    feat: improve CreateSchool

--- src/praktica/views/CreateSchool.js
+++ src/praktica/views/CreateSchool.js
@@ -1,7 +1,68 @@
+import { Input } from 'reactstrap';
+
+const inputs = [
+  {
+    name: 'name',
+    type: 'text',
+    placeholder: 'School name',
+  },
+  {
+    name: 'category',
+    type: 'text',
+    placeholder: 'Category',
+  },
+  {
+    name: 'IBAN',
+    type: 'text',
+    placeholder: 'IBAN',
+  },
+  {
+    name: 'address',
+    type: 'text',
+    placeholder: 'Address',
+  },
+  {
+    name: 'phone',
+    type: 'text',
+    placeholder: 'Phone',
+  },
+  {
+    name: 'email',
+    type: 'text',
+    placeholder: 'Email',
+  },
+  {
+    name: 'address',
+    type: 'text',
+    placeholder: 'Address',
+  },
+  {
+    name: 'city',
+    type: 'text',
+    placeholder: 'City',
+  },
+  {
+    name: 'country',
+    type: 'text',
+    placeholder: 'Country',
+  },
+];
+
 export function CreateSchool() {
   return (
-    <div>
+    <div style={{ width: '70%', marginInline: 'auto', paddingTop: 200 }}>
       <h1>Register School</h1>
+      <div className='d-flex flex-column gap-3'>
+        {inputs.map((input, index) => (
+          <Input
+            key={index}
+            name={input.name}
+            type={input.type}
+            placeholder={input.placeholder}
+          />
+        ))}
+        <button className='btn btn-primary'>Create</button>
+      </div>
     </div>
   );
 }
